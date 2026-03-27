9d19da8c585d3dae8e77b17cfc66a9f27c440695
Angel Merino <acedeno00@gmail.com>
2026-03-27 10:56:27 -0700

    quarto y credenciales

--- src/pulse/config/settings.py
+++ src/pulse/config/settings.py
@@ -3,3 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 powerbi_frame = os.getenv("POWERBI_IFRAME_URL")
+
+ip_dev = os.getenv("IP_DEV")
+port_dev = os.getenv("PORT_DEV")
+user_dev = os.getenv("USER_DEV")
+pwd_dev = os.getenv("PWD_DEV")
+db_dev = os.getenv("DB_DEV")
