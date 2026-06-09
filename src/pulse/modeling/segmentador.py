5df0fce99b53007ae9402375fab30caab8299e5f
Angel Merino <acedeno00@gmail.com>
2026-06-09 11:03:51 -0700

    hot fix

--- src/pulse/modeling/segmentador.py
+++ src/pulse/modeling/segmentador.py
@@ -221,7 +221,7 @@ class SegmentadorClientes:
         self._validate_input(df)
 
         X = df[self.metadata.features].copy()
-        clusters = cast(NDArray, self.pipeline.predict(X))
+        clusters = self.pipeline.predict(X)
 
         out = df.copy()
         out["cluster_id"] = clusters
